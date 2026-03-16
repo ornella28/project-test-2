f90725d2fe3b3c5402b228f31eb43263e69ebd52
ornella28 <angelnibaruta88@gmail.com>
2026-03-16 14:21:00 +0100

    CREATED Plan and Subscription entities

--- subscription-api/src/main/java/se/lexicon/subscriptionapi/domain/entity/Plan.java
+++ subscription-api/src/main/java/se/lexicon/subscriptionapi/domain/entity/Plan.java
@@ -1,4 +1,43 @@
 package se.lexicon.subscriptionapi.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "plans")
 public class Plan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ServiceType serviceType;
+
+    @Column(nullable = false)
+    private boolean active;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
 }
--- subscription-api/src/main/java/se/lexicon/subscriptionapi/domain/entity/Subscription.java
+++ subscription-api/src/main/java/se/lexicon/subscriptionapi/domain/entity/Subscription.java
@@ -1,4 +1,45 @@
 package se.lexicon.subscriptionapi.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "subscriptions")
 public class Subscription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "plan_id", nullable = false)
+    private Plan plan;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private SubscriptionStatus status;
+
+    @Column(nullable = false)
+    private LocalDateTime startDate;
+
+    private LocalDateTime cancellationDate;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
 }
