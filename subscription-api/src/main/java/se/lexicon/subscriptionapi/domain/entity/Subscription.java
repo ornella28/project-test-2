fe3bdedbf284e882bb1f616b49ca32e7b7825e13
ornella28 <angelnibaruta88@gmail.com>
2026-03-18 13:28:11 +0100

    CREATED SubscriptionImplService and corrected some errors in Subscription entity

--- subscription-api/src/main/java/se/lexicon/subscriptionapi/domain/entity/Subscription.java
+++ subscription-api/src/main/java/se/lexicon/subscriptionapi/domain/entity/Subscription.java
@@ -1,10 +1,7 @@
 package se.lexicon.subscriptionapi.domain.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import se.lexicon.subscriptionapi.domain.constant.SubscriptionStatus;
 
 import java.time.LocalDateTime;
@@ -14,6 +11,7 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Table(name = "subscriptions")
 public class Subscription {
 
